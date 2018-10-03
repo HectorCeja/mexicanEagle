d1cf222d02b98f5907c319c9c92aac2bce9b4b38
Juan Daniel Eseberre Torres <14170899@itculiacan.edu.mx>
2018-10-02 21:20:03 -0600

    Se agrego informacion en acerca de

--- views/site/about.php
+++ views/site/about.php
@@ -4,15 +4,27 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'RopaLinda';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
+    <h3>Empresa 100 % sinaloense dedicada al diseño, producción, venta y distribución de ropa para hombres y mujeres desde marzo del 2009 ofreciendo producto y servicio de alta calidad a un precio justo. </h3>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+    <h2>Misión</h2>
+    <h3>Ofrecer el mejor producto y servicio que tengan calidad y valor para el cliente</h3>
+    <h2>Visión</h2>
+    <h3>Ser la marca de ropa preferida que vista a todo el mundo por su calidad y comodidad.</h3>
 
-    <code><?= __FILE__ ?></code>
+   <h2>Valores</h2>
+    <ol>
+    <li><h4>Responsabilidad social</h4></li> 
+    <li><h4>Transparencia y Honestidad</h4></li> 
+    <li><h4>Respeto</h4></li> 
+    <li><h4>Equidad</h4></li> 
+    <li><h4>Empatía</h4></li> 
+    </ol>
 </div>
+
+
+
