2f92b3c7f3ae68af270ed9ff6f6c880f72a308ca
HectorCeja <hcejag@concredito.com.mx>
2018-10-07 18:53:38 -0600

    se agregan iconos y se cambia el nombre de db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=example',
+    'dsn' => 'mysql:host=localhost;dbname=ropalindaDB',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
