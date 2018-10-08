71aaab41235b77dca587348c12199ae76fdf43f2
HectorCeja <hcejag@concredito.com.mx>
2018-10-07 18:14:27 -0600

    se mejora namespace usuario

--- views/site/index.php
+++ views/site/index.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 
-//$this->title = 'Inicio';
+$this->title = 'Inicio';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-index mainContainer">
