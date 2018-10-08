48a9c4887fe85916e7ef5313026752213bdf0529
HectorCeja <hcejag@concredito.com.mx>
2018-10-07 20:31:58 -0600

    se mejora diseño de abot

--- views/site/about.php
+++ views/site/about.php
@@ -8,22 +8,37 @@ $this->title = 'RopaLinda';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <h3>Empresa 100 % sinaloense dedicada al diseño, producción, venta y distribución de ropa para hombres y mujeres desde marzo del 2009 ofreciendo producto y servicio de alta calidad a un precio justo. </h3>
-
-    <h2>Misión</h2>
-    <h3>Ofrecer el mejor producto y servicio que tengan calidad y valor para el cliente</h3>
-    <h2>Visión</h2>
-    <h3>Ser la marca de ropa preferida que vista a todo el mundo por su calidad y comodidad.</h3>
-
-   <h2>Valores</h2>
-    <ol>
-    <li><h4>Responsabilidad social</h4></li> 
-    <li><h4>Transparencia y Honestidad</h4></li> 
-    <li><h4>Respeto</h4></li> 
-    <li><h4>Equidad</h4></li> 
-    <li><h4>Empatía</h4></li> 
-    </ol>
+    <div class="jumbotron">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <p>Empresa 100% sinaloense dedicada al diseño, producción, venta y distribución de ropa para hombres y mujeres desde marzo del 2009, ofreciendo producto y servicio de alta calidad a un precio justo.</p>
+    </div>
+    <div class="jumbotron">
+        <h1>Misión</h1>
+        <p>Ofrecer el mejor producto y servicio que tengan calidad y valor para el cliente.</p>
+    </div>
+    <div class="jumbotron">
+        <h1>Visión</h1>
+        <p>Ser la marca de ropa preferida que vista a todo el mundo por su calidad y comodidad.</p>
+    </div>
+    <div class="jumbotron">
+        <h1>Valores</h1>
+        <p>
+            <h4>Responsabilidad social</h4>
+            <h4>Transparencia y Honestidad</h4>
+            <h4>Respeto</h4>
+            <h4>Equidad</h4>
+            <h4>Empatía</h4> 
+        
+        </p>
+    </div>
+    <style type="text/css">
+        .jumbotron{
+            margin-top:20px;
+            background-color:#5fcc9c;
+            color: white;
+        }
+    </style>
+    
 </div>
 
 
