b55f173e88e9720229bd1b78f9c3dcf5fa0d75c2
Paul Burgueño <jpburguenos@concredito.com.mx>
2018-12-09 18:37:24 -0700

    conflictos

--- config/db.php
+++ config/db.php
@@ -4,11 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost:3560;dbname=ropalindadb',
     'username' => 'root',
-<<<<<<< HEAD
     'password' => '1234',
-=======
-    'password' => '12345678',
->>>>>>> 11b823a4c96e55abe7f4ca2fb6cb4988b676b303
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
